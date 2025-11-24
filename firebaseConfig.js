a15f06b20b8b80309ddff434a9ef0c230b2af4de
yanirarios <abrildelyanirachanmin226@gmail.com>
2025-11-24 20:02:29 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,1 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "XXXXXXXXXX",
+  appId: "1:XXXXXXXXXX:web:XXXXXXXXXX"
+};
 
