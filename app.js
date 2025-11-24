79f71aadd559e85aec81f764bbd3827a2f18f775
yanirarios <abrildelyanirachanmin226@gmail.com>
2025-11-24 20:05:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,13 @@
 import { db } from "./firebaseConfig.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Función para leer profesores
 async function cargarProfesores() {
-    const colRef = collection(db, "profesores");
-    const snapshot = await getDocs(colRef);
+  const col = collection(db, "Profesores");
+  const snapshot = await getDocs(col);
 
-    const profesores = snapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-    }));
-
-    mostrarProfesores(profesores);
+  snapshot.forEach(doc => {
+    console.log("Profesor:", doc.id, doc.data());
+  });
 }
 
-// Mostrar en pantalla
-function mostrarProfesores(lista) {
-    const contenedor = document.getElementById("lista-profesores");
-
-    contenedor.innerHTML = lista.map(prof => `
-        <div style="margin-bottom: 10px; padding: 10px; border: 1px solid #ccc">
-            <strong>${prof.Nombre} ${prof.apellido}</strong><br>
-            DNI: ${prof.DNI}<br>
-            Email: ${prof.email}<br>
-            Materias: ${prof.materias?.join(", ")}
-        </div>
-    `).join("");
-}
-
-// Llamar a la función al iniciar
 cargarProfesores();
-
